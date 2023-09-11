5c86551c533fbfe606cd391d5cbcb1dda8c71e92
tarun-medida <60532131+tarun-medida@users.noreply.github.com>
2023-09-11 21:58:02 +0530

    Setting up Win & Pause UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
 
     public float speed;
+
+    public GameObject winUI;
+    public GameObject pauseUI;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -37,13 +42,22 @@ public class PlayerController : MonoBehaviour
         {
             rigidbody2d.velocity=new Vector2(0f, 0f);
         }
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseUI.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            pauseUI.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Door")
         {
-            Debug.Log("level Complete!");
+            winUI.SetActive(true);
         }
     }
 }
