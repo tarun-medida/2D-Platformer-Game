2897ab31dd0476ed8945865fb491ce4f4c21f494
tarun-medida <60532131+tarun-medida@users.noreply.github.com>
2023-09-11 21:35:31 +0530

    Level Win Trigger & Adding Precision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public Rigidbody2D rigidbody2D;
+    Rigidbody2D rigidbody2d;
 
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,23 +19,31 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetAxis("Horizontal") > 0)
         {
-            rigidbody2D.velocity = new Vector2(speed, 0f);
+            rigidbody2d.velocity = new Vector2(speed, 0f);
         }
         else if (Input.GetAxis("Horizontal") < 0)
         {
-            rigidbody2D.velocity = new Vector2(-speed, 0f);
+            rigidbody2d.velocity = new Vector2(-speed, 0f);
         }
         else if (Input.GetAxis("Vertical") > 0)
         {
-            rigidbody2D.velocity = new Vector2(0f, speed);
+            rigidbody2d.velocity = new Vector2(0f, speed);
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
-            rigidbody2D.velocity = new Vector2(0f,-speed);
+            rigidbody2d.velocity = new Vector2(0f,-speed);
         }
         else
         {
-            rigidbody2D.velocity=new Vector2(0f, 0f);
+            rigidbody2d.velocity=new Vector2(0f, 0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Door")
+        {
+            Debug.Log("level Complete!");
         }
     }
 }
